de72f30b6431dd7760e4e0dddaa6811eea45c2eb
Varun Tiwari <tiwarivarun819.vt33.vt@gmail.com>
2021-07-13 19:48:32 +0530

    Added CORS

--- server.js
+++ server.js
@@ -1,10 +1,12 @@
 require('dotenv').config()
 
 const PORT = process.env.PORT || 5000
+const cors = require('cors')
 const express = require('express')
 const app = express()
 const auth = require('./routes/auth')
 
+app.use(cors())
 app.use(express.json())
 
 app.use('/auth', auth)
